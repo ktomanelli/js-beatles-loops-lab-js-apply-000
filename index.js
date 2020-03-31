ff27edf8a178887e0e59116c86fb9a40e5040605
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-03-31 00:41:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // add solution here
 
-function theBeatlesPlay()
+function theBeatlesPlay(musicians,instruments){
+  var arr=[];
+  for(let i=0;i<arr.length;i+=1){
+    arr.push(musicians[i]+' plays '+instruments[i]);
+  }
+  return arr;
+}
