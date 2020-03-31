72a1f540d28ba299c1b8b8b5851d9353f5023861
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-03-31 00:44:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 // add solution here
 
+
 function theBeatlesPlay(musicians,instruments){
   var arr=[];
   for(let i=0;i<arr.length;i+=1){
     arr.push(`${musicians[i]} plays ${instruments[i]}`);
   }
   return arr;
+}
+
+function johnLennonFacts(facts){
+  let i=0;
+  while(i<facts.length){
+    facts[i] += '!!!';
+    i++;
+  }
 }
